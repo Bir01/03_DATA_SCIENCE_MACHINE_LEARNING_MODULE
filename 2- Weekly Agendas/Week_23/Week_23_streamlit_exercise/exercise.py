69c1d7a3cb098f6703bc7c3a2d362f8d16cb8d93
bir01 <katarbir@gmail.com>
2022-10-05 17:40:51 +0200

    Create exercise.py

---
+++ 2- Weekly Agendas/Week_23/Week_23_streamlit_exercise/exercise.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
+from PIL import Image
+st.title("Streamlit Tutorials")
+st.text("Hello Streamlit")
+st.header("This is a header")
+st.subheader("This is a subheader")
